b1fbabf328b1a2c13987d4c18ce3ea1bfe75a6e7
soheil_dev <soheil.torkan2222@gmail.com>
2026-02-08 16:49:22 +0330

    finish create comment component

--- src/layout/pages/comments/createComment.jsx
+++ src/layout/pages/comments/createComment.jsx
@@ -1,12 +1,37 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router"
 import { MyContext } from "../../../context";
+import EmptyInputs from "../../../emtyError";
+import { postComment } from "../../../services/comments";
 
 export default function CreateComment() {
     const navigate = useNavigate();
     const { isOpenModal, setModal } = useContext(MyContext);
     const nameInput = useRef(null);
     const bodyInput = useRef(null);
+    const [datas, setDatas] = useState({});
+    const [isError, setError] = useState(false);
+    const handleClickButton = (e) => {
+        e.preventDefault();
+        const name = nameInput.current.value.trim();
+        const body = bodyInput.current.value.trim();
+        if (!name || !body) {
+            setError(true);
+            setTimeout(() => {
+                setError(false);
+            }, 5000);
+        }
+        else {
+            const objDatas = { name: name, body: body };
+            setDatas(objDatas);
+            postComment(datas);
+        }
+
+    }
+    const sendByEnter = (e) => {
+        if (e.key === "Enter")
+            handleClickButton();
+    }
     const handleCloseModal = () => {
         navigate("/comments");
         setModal(false);
@@ -26,13 +51,14 @@ export default function CreateComment() {
                         <i className="fi fi-rs-circle-xmark"></i>
                     </button>
                     <h3 className="text-3xl font-bold text-center dark:text-white mb-6">افزودن نظر جدید</h3>
+                    {isError && <EmptyInputs />}
                     <input type="text" className="w-full p-3 rounded-lg border focus:outline-none "
                         placeholder="نام..." ref={nameInput} />
                     <input type="text" className="w-full p-3 rounded-lg border focus:outline-none "
                         placeholder="متن نظر..." ref={bodyInput} />
                     <input type="submit" className="w-36 rounded-lg p-3 cursor-pointer
                         transition-all duration-300 text-white bg-blue-400 hover:bg-blue-500 text-lg"
-                        value="افزودن نظر +" />
+                        value="افزودن نظر +" onClick={handleClickButton} />
                 </div>
             </form>
         </div>
